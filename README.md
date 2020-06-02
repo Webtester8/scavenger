3c5918743af0d2c1c6f9335e42829b7a38e76c07
Web Tester <testingtheweb8@gmail.com>
2020-06-02 13:57:09 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Scavenger
+Scavenger is an application used for brute forcing directories very fast with big lists to use.
+
+## Install
+Unfortunately, it isn't all that easy right now to download and use since it is still under major development.
+
+1. Download the all the files
+2. Make sure you have go installed
+
+## Run
+To run, do the following in your command line.
+ ```sh
+ cd $PATH/scavenger-master/
+ go run scavenger.go -h
+ ```
+ ## Flags
+
+ There are a couple of flags you can use that may help you.
+
+ #### Help(-h)
+ Displays help menu
+ ```sh
+  go run scavenger.go -h
+ ````
+ #### Wordlist(-w)*
+ This is used to tell where the wordlist are
+ ```sh
+go run scavenger.go -w all.txt -u example.com
+ ```
+ #### URL(-u)*
+ Enter the base url to tell what site to test.
+ ```sh
+go run scavenger.go -w all.txt -u example.com
+ ```
+#### Verbose(-v)*
+Verbose or display everything the script is doing and what it gets.
+ ```sh
+go run scavenger.go -w all.txt -u example.com -v true
+ ```
+
+
+
+ *Required to run
---
+++ scavenger.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"runtime"
+	"sync"
+)
+
+//global varibels
+var Output bool = false
+var Ht string = "http://"
+var Verbose bool = false
+
+func main() {
+	//Start wait WaitGroup
+	var wg sync.WaitGroup
+
+	//load all flags
+	list := flag.String("w", "", "Path to the word list")
+	url := flag.String("u", "", "Base of the url to be bruted(EX: google.com)")
+	ofile := flag.String("o", "", "Path to output file")
+	ver := flag.String("v", "", "Verbose activity")
+
+	//Parse all of the flags
+	flag.Parse()
+
+	//Test all of the flags
+	if *list == "" {
+		if *url == "" {
+			panic("No list(-w) or url(-u) given. Use -h for help.")
+		} else {
+			panic("No list(-w) for brute force given. Use -h for help.")
+		}
+	}
+	if *url == "" {
+		panic("No url(-u) given. Use -h for help.")
+	}
+	if *ofile != "" {
+		Output = true
+	}
+	if *ver != "" {
+		Verbose = true
+	}
+	//See how many threads a computer has and add them to wait WaitGroup
+	wg.Add(runtime.NumCPU())
+
+	//Test for valid url
+	_, err := http.Get("http://" + *url)
+	if err != nil {
+		panic(`
+      Not a valid url
+          [Hint] Make sure you DON'T have "http://www." in your url
+          [Hint] If you don't have internet, this also can be triggered`)
+	}
+
+	//Test if https works
+	_, err = http.Get("https://" + *url)
+	if err == nil {
+		Ht = "https://"
+	}
+
+	//Test for valid wordlist
+	test, err := os.Open(*list)
+	if err != nil {
+		panic(`Not a valid wordlist
+              [Hint] Make sure your give the WHOLE file name end extension
+              [Hint] Make sure that that is a direct path from the file your in`)
+	}
+	test.Close()
+	//Test or make Outputfile
+	if Output == true {
+		test, err = os.Open(*ofile)
+		if err != nil {
+			test, _ := os.Create(*ofile)
+			test.Close()
+		}
+		test.Close()
+	}
+	r, _ := os.Open(*list)
+
+	defer r.Close()
+
+	//Prep Scanner
+	s := bufio.NewScanner(r)
+
+	//Prep Bruter
+	var bru []string
+	var n int = 0
+	ur := (Ht + *url + "/")
+	for s.Scan() {
+		bru = append(bru, s.Text())
+		n++
+	}
+	//Divid up the wordlist to each thread
+	spacer := n / runtime.NumCPU()
+
+	// Get invalid method
+	rt, _ := http.Get(ur + "/ubgkvjkgfvjhvjvhjchkcjhbkjgvkhio7yi86tr65dcj")
+	if rt.StatusCode == 200 {
+		fmt.Println("Brute force maybe ineffective against target.")
+		fmt.Println(ur + "/ubgkvjkgfvjhvjvhjchkcjhbkjgvkhio7yi86tr65dcj returned status code: " + rt.Status)
+	}
+	invalid := rt.StatusCode
+	//Brute force url and send them to Output (if selected)
+	n = runtime.NumCPU()
+	start := 0
+	end := spacer
+	fmt.Println("Starting bruteforce...")
+	if Verbose == true {
+		for n != 0 {
+			go vBrute(bru, ur, invalid, start, end, &wg)
+			n--
+			start = start + spacer
+			end = end + spacer
+		}
+	} else {
+		for n != 0 {
+			go brute(bru, ur, invalid, start, end, &wg)
+			n--
+			start = start + spacer
+			end = end + spacer
+		}
+	}
+	wg.Wait()
+}
+
+//Brute with Verbose
+func vBrute(bru []string, ur string, invalid int, start int, stop int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for n, text := range bru {
+		if n >= start {
+			if n <= stop {
+				re, _ := http.Get(ur + text)
+				fmt.Println(ur + text + " => " + re.Status)
+				if re.StatusCode != invalid {
+					fmt.Println(ur + text)
+				}
+			}
+		}
+	}
+}
+
+//Normal Brute
+func brute(bru []string, ur string, invalid int, start int, stop int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for n, text := range bru {
+		if n >= start {
+			if n <= stop {
+				re, _ := http.Get(ur + text)
+				fmt.Println(ur + text + " => " + re.Status)
+				if re.StatusCode != invalid {
+					fmt.Println(ur + text)
+				}
+			}
+		}
+	}
+}
